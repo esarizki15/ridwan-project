7825028d694a87a06f3c36a4ff5b4e9cdb54e409
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2021-01-16 21:14:52 +0700

    up

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,5 +1,6 @@
+@if(!$isStack)
 <div class="form-group row">
-    <label for="{{ $globalAttribute }}" class="col-md-4 col-form-label text-md-right">{{ __($label ? $label : ucwords($globalAttribute)) }}</label>
+    <label for="{{ $globalAttribute }}" class="col-md-4 col-form-label text-md-right">{{ __($label ? ucwords($label) : ucwords($globalAttribute)) }}</label>
 
     <div class="col-md-6">
         <input id="{{ $globalAttribute }}" type="{{ $type }}" class="form-control @error($globalAttribute) is-invalid @enderror" name="{{ $globalAttribute }}" value="{{ $defaultValue }}" {{ $customAttribute }} autocomplete="{{ $globalAttribute }}" autofocus>
@@ -10,3 +11,14 @@
         @enderror
     </div>
 </div>
+@else
+<div class="form-group row">
+    <label for="{{ $globalAttribute }}">{{ __($label ? ucwords($label) : ucwords($globalAttribute)) }}</label>
+    <input id="{{ $globalAttribute }}" type="{{ $type }}" class="form-control @error($globalAttribute) is-invalid @enderror" name="{{ $globalAttribute }}" value="{{ $defaultValue }}" {{ $customAttribute }} autocomplete="{{ $globalAttribute }}" autofocus>
+    @error($globalAttribute)
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
+</div>
+@endif
