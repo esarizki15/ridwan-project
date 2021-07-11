dc73fde5604a292a1b526ae8086d1a833b25573b
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2021-07-11 09:57:43 +0700

    up

--- resources/views/components/action.blade.php
+++ resources/views/components/action.blade.php
@@ -2,7 +2,7 @@
     <form method="POST" action="{{ route($route.'.destroy', $object->id) }}">
         {{ csrf_field() }}
         {{ method_field('DELETE') }}
-        
+        {{ $slot }}        
         <a href="{{ route($route.'.edit', $object->id) }}" class="btn btn-xs btn-primary">Edit</a>
         <button class="btn btn-xs btn-danger">Hapus</button>
     </form>
